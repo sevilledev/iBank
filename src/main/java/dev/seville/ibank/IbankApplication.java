08adf517fc755385a71ef33495d0aeec7043aa86
sevilledev <sevillerasulova@gmail.com>
2025-11-10 14:32:31 +0400

    feat(core): init project with dependencies and DB config

---
+++ src/main/java/dev/seville/ibank/IbankApplication.java
@@ -0,0 +1,12 @@
+package dev.seville.ibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbankApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(IbankApplication.class, args);
+	}
+}
